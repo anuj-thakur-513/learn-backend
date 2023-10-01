721ed38a9da4c476bc23c23b26e83ae51ba91166
Anuj Thakur <anujthakur2003.anuj@gmail.com>
2023-10-01 23:05:55 +0530

    filtering out specific data

--- planets-project/index.js
+++ planets-project/index.js
@@ -1,7 +1,16 @@
 const { parse } = require("csv-parse");
 const fs = require("fs");
 
-const results = [];
+const habitablePlanets = [];
+
+const isHabitablePlanet = function (planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+};
 
 fs.createReadStream("kepler-data.csv")
   .pipe(
@@ -11,12 +20,16 @@ fs.createReadStream("kepler-data.csv")
     })
   )
   .on("data", (data) => {
-    results.push(data);
+    if (isHabitablePlanet(data)) {
+      habitablePlanets.push(data);
+    }
   })
   .on("error", (e) => {
     console.log(e);
   })
   .on("end", () => {
-    console.log(results);
-    console.log("done");
+    console.log(habitablePlanets);
+    console.log(
+      `Found ${habitablePlanets.length} habitable planets like earth!!!`
+    );
   });
