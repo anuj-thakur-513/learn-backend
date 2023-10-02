1b63244dc9c31ebdf225e5bc46eae28c97c65339
Anuj Thakur <anujthakur2003.anuj@gmail.com>
2023-10-02 17:16:46 +0530

    using parameterized URLs

--- http-server/index.js
+++ http-server/index.js
@@ -1,19 +1,37 @@
 const http = require("http");
 
 const PORT = 3000;
-
 const server = http.createServer();
 
-server.on("request", (req, res) => {
-  if (req.url === "/friends") {
-    // res.writeHead(200, {
-    //   "Content-Type": "application/json",
-    // });
+const friends = [
+  {
+    id: 0,
+    name: "Nikola Tesla",
+  },
+  {
+    id: 1,
+    name: "Albert Einstein",
+  },
+  {
+    id: 2,
+    name: "Sir Isaac Newton",
+  },
+];
 
+server.on("request", (req, res) => {
+  const items = req.url.split("/");
+
+  if (items[1] === "friends") {
     res.statusCode = 200;
     res.setHeader("Content-Type", "application/json");
-    res.end(JSON.stringify({ id: 1, name: "Sir Issac Newton" }));
-  } else if (req.url === "/messages") {
+
+    if (items.length === 3) {
+      const friendIndex = Number(items[2]);
+      res.end(JSON.stringify(friends[friendIndex]));
+    } else {
+      res.end(JSON.stringify(items));
+    }
+  } else if (items[1] === "messages") {
     res.setHeader("Content-Type", "text/html");
     res.write("<html>");
     res.write("<body>");
