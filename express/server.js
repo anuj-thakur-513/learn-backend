6a93c291b82fc6fd3ff5cae2f9bd3f7e174364b4
Anuj Thakur <anujthakur2003.anuj@gmail.com>
2023-10-03 09:32:48 +0530

    adding parameterized routes

--- express/server.js
+++ express/server.js
@@ -3,11 +3,31 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send({
-    id: 1,
+const friends = [
+  {
+    id: 0,
     name: "Nikola Tesla",
-  });
+  },
+  {
+    id: 1,
+    name: "Sir Isaac Newton",
+  },
+];
+
+app.get("/", (req, res) => {
+  res.json(friends);
+});
+
+app.get("/friends/:friendId", (req, res) => {
+  const friendId = +req.params.friendId; // converting to number using '+' sign
+  const friend = friends[friendId];
+  if (friend) {
+    res.json(friend);
+  } else {
+    res.status(404).json({
+      error: "Friend does not exist",
+    });
+  }
 });
 
 app.get("/messages", (req, res) => {
