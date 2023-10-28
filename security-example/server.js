c5c9eb990700a63f75d25c12b770e3b2d056df47
Anuj Thakur <anujthakur2003.anuj@gmail.com>
2023-10-28 18:22:12 +0530

    generating self signed cert and adding helmet.js

--- security-example/server.js
+++ security-example/server.js
@@ -1,8 +1,18 @@
+const fs = require("fs");
 const path = require("path");
+const https = require("https");
+const helmet = require("helmet");
 const express = require("express");
 
 const app = express();
 const PORT = 3000;
+
+const options = {
+  key: fs.readFileSync("./key.pem"),
+  cert: fs.readFileSync("./cert.pem"),
+};
+
+app.use(helmet());
 
 app.get("/", (req, res) => {
   return res.sendFile(path.join(__dirname, "public", "index.html"));
@@ -12,6 +22,6 @@ app.get("/secret", (req, res) => {
   return res.send("Your personal secret value is 42!");
 });
 
-app.listen(PORT, () => {
+https.createServer(options, app).listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
