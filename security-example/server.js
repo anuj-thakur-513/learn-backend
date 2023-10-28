2bcd0f72a05d6cff2e2851b54c75754f531ac016
Anuj Thakur <anujthakur2003.anuj@gmail.com>
2023-10-28 19:16:06 +0530

    adding client details for google auth 2.0

--- security-example/server.js
+++ security-example/server.js
@@ -3,9 +3,14 @@ const path = require("path");
 const https = require("https");
 const helmet = require("helmet");
 const express = require("express");
+require("dotenv").config();
 
 const app = express();
 const PORT = 3000;
+const config = {
+  CLIENT_ID: process.env.GOOGLE_AUTH_CLIENT_ID,
+  CLIENT_SECRET: process.env.GOOGLE_AUTH_CLIENT_SECRET,
+};
 
 const options = {
   key: fs.readFileSync("./key.pem"),
@@ -14,11 +19,28 @@ const options = {
 
 app.use(helmet());
 
+// middleware to check if the user is logged in
+function checkLoggedIn(req, res, next) {
+  const isLoggedIn = true; //TODO
+  if (!isLoggedIn) {
+    return res.status(401).json({
+      error: "You must log in",
+    });
+  }
+  next();
+}
+
 app.get("/", (req, res) => {
   return res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.get("/secret", (req, res) => {
+app.get("/auth/google", (req, res) => {});
+
+app.get("/auth/google/callback", (req, res) => {});
+
+app.get("/auth/logout", (req, res) => {});
+
+app.get("/secret", checkLoggedIn, (req, res) => {
   return res.send("Your personal secret value is 42!");
 });
 
