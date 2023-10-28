676321373704312a2f269e192515cf394c87739f
Anuj Thakur <anujthakur2003.anuj@gmail.com>
2023-10-28 19:37:46 +0530

    adding google authentication using passport

--- security-example/server.js
+++ security-example/server.js
@@ -3,6 +3,9 @@ const path = require("path");
 const https = require("https");
 const helmet = require("helmet");
 const express = require("express");
+const passport = require("passport");
+const { Strategy } = require("passport-google-oauth20");
+
 require("dotenv").config();
 
 const app = express();
@@ -12,12 +15,26 @@ const config = {
   CLIENT_SECRET: process.env.GOOGLE_AUTH_CLIENT_SECRET,
 };
 
+const AUTH_OPTIONS = {
+  callbackURL: "/auth/google/callback",
+  clientID: config.CLIENT_ID,
+  clientSecret: config.CLIENT_SECRET,
+};
+
+function verifyCallback(accessToken, refreshToken, profile, done) {
+  console.log("Google profile: ", profile);
+  done(null, profile);
+}
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
+
 const options = {
   key: fs.readFileSync("./key.pem"),
   cert: fs.readFileSync("./cert.pem"),
 };
 
 app.use(helmet());
+app.use(passport.initialize());
 
 // middleware to check if the user is logged in
 function checkLoggedIn(req, res, next) {
@@ -34,14 +51,33 @@ app.get("/", (req, res) => {
   return res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.get("/auth/google", (req, res) => {});
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["email"],
+  })
+);
 
-app.get("/auth/google/callback", (req, res) => {});
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/failure",
+    successRedirect: "/",
+    session: false,
+  }),
+  (req, res) => {
+    console.log("Google called us back!");
+  }
+);
 
 app.get("/auth/logout", (req, res) => {});
 
 app.get("/secret", checkLoggedIn, (req, res) => {
   return res.send("Your personal secret value is 42!");
+});
+
+app.get("/failure", (req, res) => {
+  return res.send("Failed to Login");
 });
 
 https.createServer(options, app).listen(PORT, () => {
